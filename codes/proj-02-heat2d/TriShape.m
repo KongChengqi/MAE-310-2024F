de699c3211b082f582fb1f6e4fcec929e5fa2be7
KongChengqi <3343036186@qq.com>
2024-12-30 23:55:25 +0800

    分别计算三角形形函数及其梯度

--- codes/proj-02-heat2d/TriShape.m
+++ codes/proj-02-heat2d/TriShape.m
@@ -1,14 +1,11 @@
-function [val, grad_xi_eta] = TriShape(aa, xi, eta)
-    if aa == 1
-        val = 1 - xi - eta;
-        grad_xi_eta = [-1; -1];
+function val = TriShape(aa, xi, eta)
+
+if aa == 1
+    val = 1 - xi - eta;
     elseif aa == 2
-        val = xi;
-        grad_xi_eta = [1; 0];
+    val = xi;
     elseif aa == 3
-        val = eta;
-        grad_xi_eta = [0; 1];
+    val = eta;
     else
-        error('Node index should be 1, 2, or 3');
-    end
+    error('Error: value of a should be 1, 2,or 3.');
 end
