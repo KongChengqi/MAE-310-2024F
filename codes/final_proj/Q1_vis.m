41d5560de3a454a9647ce116b060196e6cfd67f7
KongChengqi <3343036186@qq.com>
2025-01-10 23:03:35 +0800

    对可视化添加了一些说明

--- codes/final_proj/Q1_vis.m
+++ codes/final_proj/Q1_vis.m
@@ -9,9 +9,10 @@ n_np_y = n_el_y + 1;
 
 [X, Y] = meshgrid(0 : hh_x : 1, 0 : hh_y : 1);
 Z = reshape(disp(:,1), n_np_x, n_np_y)';
+%想看disp第二列数据导出的云图的话可以改成Z = reshape(disp(:,2), n_np_x, n_np_y)';
 surf(X, Y, Z);
 
 shading interp;
-az = -61;
 el = 20;
+az = -61;
 view(az,el);
